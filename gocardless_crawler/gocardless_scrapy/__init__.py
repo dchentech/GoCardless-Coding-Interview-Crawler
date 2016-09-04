85ebe93790b5930080669f19ccb3ee194da6022f
David Chen <mvjome@gmail.com>
2016-09-04 13:52:13 +0100

    Refactor scrapy, separate main function into small and readable functions

--- gocardless_crawler/gocardless_scrapy/__init__.py
+++ gocardless_crawler/gocardless_scrapy/__init__.py
@@ -1,7 +1,6 @@
 # -*-coding:utf-8-*-
 
 import os
-import sys
 from .spider import Spider
 from .http_request import Request
 from Queue import Queue
@@ -17,50 +16,62 @@ class scrapy(object):
     @classmethod
     def run(cls, crawler_recipe):
         print "... begin to run ", crawler_recipe
+        crawler = cls(crawler_recipe)
+        crawler.process()
 
-        instance = crawler_recipe()
+    def __init__(self, crawler_recipe):
+        self.crawler_recipe = crawler_recipe
 
+        # https://docs.python.org/2/library/queue.html
+        # TODO maybe optimize maxsize
+        self.job_queue = Queue()  # It's thread-safe
+
+        self.instance = self.crawler_recipe()
+
+    def process(self):
+        self.start_worker_threads()
+        self.fetch_init_urls()
+        self.check_if_job_is_done()
+
+    def fetch_init_urls(self):
+        for url in self.instance.start_urls:
+            self.job_queue.put(Request(url, self.instance.parse))
+        for request in self.instance.start_requests():
+            self.job_queue.put(request)
+        assert self.job_queue.qsize() != 0
+        # job_queue.join()  # block until all tasks are done
+
+    def check_if_job_is_done(self):
+        while True:
+            time.sleep(scrapy.thread_check_queue_finished_seconds)
+            # If we process the last item, then exit.
+            if self.job_queue.empty():
+                print "[thread %s] exits ..." % threading.current_thread().name
+                os._exit(0)
+
+    def start_worker_threads(self):
+        print "2. Create %s threads ..." % self.thread_count
+        for idx in xrange(self.thread_count):
+            print "create thread[%s] ..." % (idx + 1)
+            t = threading.Thread(target=self.worker_func(),
+                                 args=(self.job_queue,))
+            t.start()
+
+# TODO dead link
+    def worker_func(self):
         def worker(q):
             print "[thread %s] starts ..." % threading.current_thread().name
 
             while True:
                 time.sleep(scrapy.thread_sleep_seconds)
-                if not job_queue.empty():
-                    item = job_queue.get()
-                    print "item: ", item  # process item
-                    job_queue.task_done()
+                if not q.empty():
+                    item = q.get()
+                    if item is not None:
+                        print "item: ", item  # process item
+                        q.task_done()
 
-                    print "Current job_queue.size is %s ..." % \
-                          job_queue.qsize()
-
-        # https://docs.python.org/2/library/queue.html
-        # TODO maybe optimize maxsize
-        print "1. Init job_queue ..."
-        job_queue = Queue()  # It's thread-safe
-
-        print "2. Create %s threads ..." % cls.thread_count
-        for idx in xrange(cls.thread_count):
-            print "create thread[%s] ..." % (idx + 1)
-            t = threading.Thread(target=worker, args=(job_queue,))
-            t.start()
-
-        print "3. Use a single thread to get all urls ..."
-        for url in instance.start_urls:
-            job_queue.put(Request(url, instance.parse))
-        for request in instance.start_requests():
-            job_queue.put(request)
-        assert job_queue.qsize() != 0
-
-        # job_queue.join()  # block until all tasks are done
-
-        while True:
-            time.sleep(scrapy.thread_check_queue_finished_seconds)
-            # If we process the last item, then exit.
-            if job_queue.empty():
-                print "[thread %s] exits ..." % threading.current_thread().name
-                os._exit(0)
-
-# TODO dead link
-
+                    print "Current queue size is %s ..." % \
+                          q.qsize()
+        return worker
 
 __all__ = ['scrapy', 'Request']
