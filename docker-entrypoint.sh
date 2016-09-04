ca4fb5bee018631e55e90d0e037ece1f6c680a44
David Chen <mvjome@gmail.com>
2016-09-04 09:58:53 +0100

    Support tox

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,9 @@ SCRAPY_OUTPUT_JSON=output/scrapy_gocardless.json
 
 rm -f $SCRAPY_OUTPUT_JSON
 
-pip install -r requirements.txt
+pip install -U tox
+tox
+#pip install -r requirements.txt
 
 scrapy runspider gocardless_crawler/crawler.py --output $SCRAPY_OUTPUT_JSON
 ./bin/clean_result.py $SCRAPY_OUTPUT_JSON $RESULT_JSON
